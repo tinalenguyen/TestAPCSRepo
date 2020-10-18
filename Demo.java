2e99d19e374ac8eddb648c879fbdca3b6e36e276
Tina Nguyen <thetinalenguyen@gmail.com>
2020-10-18 01:34:36 -0400

    I tested this program and it works now

---
+++ Demo.java
@@ -0,0 +1,25 @@
+public class Demo {
+
+public static void main(String[] args){
+
+  printLoop();
+
+}
+
+public static void printLoop(){
+
+
+for (int i = 1; i <= 5; i++){
+
+  for (int j = 5; j >= i; j--){
+    System.out.print(i);
+
+  }
+
+System.out.println();
+}
+
+
+}
+
+}
